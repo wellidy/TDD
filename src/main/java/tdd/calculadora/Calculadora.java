65ade50230626325c88c54bfa0fb40aa17604a96
Wellidy Almeida <wellidy@gmail.com>
2016-09-20 16:14:23 -0300

    Inicial implementation of Calculadora

---
+++ src/main/java/tdd/calculadora/Calculadora.java
@@ -0,0 +1,18 @@
+package tdd.calculadora;
+
+public class Calculadora {
+	
+	public int soma(int vA, int vB) {
+		return vA + vB;
+		
+	}
+
+	public int subtracao(int valorA, int valorB) {		
+		return valorA - valorB;
+	}
+
+	public int divisao(int valorA, int valorB) {
+		return valorA/valorB;
+	}
+
+}
